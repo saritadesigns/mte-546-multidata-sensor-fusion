e7420f1d9cabbf0d176250ebce1a7b0f61dfb24f
Sarah Mohammed <sarah.alyssa@live.com>
2021-03-03 18:18:11 -0600

    Removed files; updated velocity vector

--- lab2-extended-kalman-filter/main.m
+++ lab2-extended-kalman-filter/main.m
@@ -9,8 +9,8 @@ r = 0.4; %radius
 w = 5; %angular velocity
 
 %Errors
-Q = [0.00335, 0.00437]'; %Measurement noise [0.00335, 0.00437]';
-R = [0.05,0.05,0.01]'; %System noise %changed from 0.1 -> 0.05 for first 2 elements
+R = [0.05,0.05,0.01]'; %System noise
+Q = [0.00335, 0.00437]'; %Measurement noise
 
 % EKF Initialization
 x0 = [0 0 0]'; %initial state [x,y,theta]
@@ -29,6 +29,7 @@ x_ideal(:,1)= x0;
 % Init velocities
 v_x = r*w*cos(x(3,1));
 v_y = r*w*sin(x(3,1));
+v = [v_x v_y w]';
 
 %Storage for EKF
 mup_S = zeros(n,length(T));
@@ -36,38 +37,33 @@ mu_S = zeros(n,length(T));
 
 %% Simulation
 for t=2:length(T)
-    x_ideal(1,t) = x_ideal(1,t-1) + v_x*dt;
-    x_ideal(2,t) = x_ideal(2,t-1) + v_y*dt;
-    x_ideal(3,t) = x_ideal(3,t-1) + w*dt;
+    x_ideal(:,t) = x_ideal(:,t-1) + v(:)*dt;
     
-    x(1,t) = x(1,t-1) + v_x*dt + normrnd(0,R(1));
-    x(2,t) = x(2,t-1) + v_y*dt + normrnd(0,R(2));
-    x(3,t) = x(3,t-1) + w*dt + normrnd(0,R(3));
+    %TODO: replace with Hadibi/Eugene method
+    x(1,t) = x(1,t-1) + v(1)*dt + normrnd(0,R(1));
+    x(2,t) = x(2,t-1) + v(2)*dt + normrnd(0,R(2));
+    x(3,t) = x(3,t-1) + v(3)*dt + normrnd(0,R(3));
     
     y(:,t) = sensor_model(x_ideal(:,t));
     y(1,t) = y(1,t) + normrnd(0,Q(1));
     y(2,t) = y(2,t) + normrnd(0,Q(2));
     
-    v_x = r*w*cos(x_ideal(3,t));
-    v_y = r*w*sin(x_ideal(3,t));
+    v(1) = r*w*cos(x_ideal(3,t));
+    v(2) = r*w*sin(x_ideal(3,t));
     
     Gt = [1, 0, -sin(x_ideal(3,t-1))/5; 
           0, 1,  cos(x_ideal(3,t-1))/5;
           0, 0,1];
-
-    % Ali Update: looked into Ht format from Hadibi, this format matches
-    % it, needed to apply jacobian for all states, not just x1,x2. Take
-    % output as-is and use as Ht, no transpose needed
     
-    %Ht for poly fit % not great but way better than others
+    %Ht (poly fit)
     Ht = [(5142*x_ideal(1,t-1))/625 - 47153/10000,0,0;
         0,(23153*x_ideal(2,t-1))/5000 - 30747/10000,0];
     
-    %Ht for Inverse fit %Terrible
+    %Ht (inverse fit)
 %     Ht = [83741/(10000*(x_ideal(1,t-1) + 123/10000)) - ((83741*x_ideal(1,t-1))/10000 + 479/2000)/(x_ideal(1,t-1) + 123/10000)^2, 0, 0;
 %             0, 41779/(5000*(x_ideal(2,t-1) + 647/5000)) - ((41779*x_ideal(2,t-1))/5000 + 834/625)/(x_ideal(2,t-1) + 647/5000)^2, 0];
     
-    %Ht for power fit
+    %Ht (power fit)
 % 	Ht = [-25529417/(50000000*x_ideal(1,t-1)^(10619/10000)),0 , 0;
 %             0, -4997949/(12500000*x_ideal(2,t-1)^(1309/1250)), 0];
 
@@ -84,10 +80,11 @@ end
 
 %% Plot 
 figure(1)
+hold on
 plot(x(1,2:t),x(2,2:t), 'ro--') %state x and y (directions) for timesteps
 plot(y(1,2:t), y(2,2:t), 'x--', 'Color', '#329E2B')
 plot(mu_S(1,2:t),mu_S(2,2:t), 'bx--')
-
+hold off
 % figure(2)
 % plot(y(1,2:t), y(2,2:t), 'x--', 'Color', '#329E2B') %measurement
 
